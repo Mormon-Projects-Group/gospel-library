250f13b923e663a76769509a2b8e6544e4530286
Jared Howland <jaredhowland@Jareds-Air.lib.byu.edu>
2017-12-23 09:12:21 -0700

    Initial commit

---
+++ README.rst
@@ -0,0 +1,29 @@
+|TravisCI|_ |Scrutinizer|_ |StyleCI|_ |Packagist|_ |MIT License|_
+
+========================
+[WIP] LDS Gospel Library
+========================
+**This project is not affiliated with, nor endorsed by, the `LDS Church <https://www.lds.org/>`_.**
+
+This small PHP library allows easy access to the `Gospel Library Web Services <https://tech.lds.org/wiki/Gospel_Library_Catalog_Web_Service>`_.
+
+==========
+Contribute
+==========
+* Issue Tracker: https://github.com/Mormon-Projects-Group/gospel-library/issues
+* Source Code: https://github.com/Mormon-Projects-Group/gospel-library
+
+.. |TravisCI| image:: https://img.shields.io/travis/Mormon-Projects-Group/gospel-library/master.svg?style=flat-square
+.. _TravisCI: https://travis-ci.org/Mormon-Projects-Group/gospel-library
+
+.. |Scrutinizer| image:: https://img.shields.io/scrutinizer/g/Mormon-Projects-Group/gospel-library.svg?style=flat-square
+.. _Scrutinizer: https://scrutinizer-ci.com/g/Mormon-Projects-Group/gospel-library/
+
+.. |StyleCI| image:: https://styleci.io/repos/71304265/shield?branch=dev
+.. _StyleCI: https://styleci.io/repos/71304265
+
+.. |Packagist| image:: https://img.shields.io/packagist/v/Mormon-Projects-Group/gospel-library.svg?style=flat-square
+.. _Packagist: https://packagist.org/packages/Mormon-Projects-Group/gospel-library
+
+.. |MIT License| image:: https://img.shields.io/badge/License-MIT-blue.svg?style=flat-square
+.. _MIT License: LICENSE.rst
---
+++ src/Config.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * App configuration
+ *
+ * @author  Jared Howland <gospel-library@jaredhowland.com>
+ * @version 2017-12-23
+ * @since   2017-12-23
+ *
+ */
+
+namespace Gospel;
+
+/**
+ * Configuration class to import `Config.ini` file and set other defaults
+ */
+class Config
+{
+    /**
+     * @var array|false $config Array of values in `Config.ini`. `false` if file cannot be parsed.
+     */
+    private static $config;
+
+    /**
+     * Set error reporting based on if app is in development or production
+     *
+     * @param null
+     *
+     * @return void
+     */
+    public static function setErrorReporting()
+    {
+        if (self::get('development')) {
+            ini_set('display_errors', '1');
+            ini_set('error_reporting', E_ALL ^ E_NOTICE);
+        } else {
+            ini_set('display_errors', '0');
+        }
+    }
+
+    /**
+     * Get config setting from `.ini` file
+     *
+     * @param string $setting Setting name
+     *
+     * @return string Return setting value from `.ini` file
+     */
+    public static function get(string $setting)
+    {
+        if (empty(self::$config) && is_array(parse_ini_file('Config.ini'))) {
+            self::$config = parse_ini_file('Config.ini');
+        }
+
+        return self::settingExists($setting);
+    }
+
+    /**
+     * Check if setting in `.ini` file exists
+     *
+     * @param string $setting Setting name to check for
+     *
+     * @throws \UnexpectedValueException when `$setting` does not exist
+     *
+     * @return string Setting value from `.ini` file
+     */
+    private static function settingExists(string $setting)
+    {
+        if (isset(self::$config[$setting])) {
+            return self::$config[$setting];
+        } else {
+            throw new \UnexpectedValueException("'$setting' is not a valid config setting. Please check your 'config.ini' file for valid config options.\n");
+        }
+    }
+}
+
+config::setErrorReporting();
